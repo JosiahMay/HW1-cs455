5da36c8f2938dd3bc7247ab31dba9bcd35720dc9
Josiah May <josiahm@rams.colostate.edu>
2018-01-23 12:01:44 -0700

    Setup vars for interfaces

--- src/cs455/overlay/Node/Node.java
+++ src/cs455/overlay/Node/Node.java
@@ -2,4 +2,6 @@ package cs455.overlay.Node;
 
 public interface Node {
 
+  int MAX_NODES = 127;
+
 }
--- src/cs455/overlay/Node/Registry.java
+++ src/cs455/overlay/Node/Registry.java
@@ -1,5 +1,8 @@
 package cs455.overlay.Node;
 
-public class Registry {
+public class Registry implements Node{
 
+  public static void main(String[] args) {
+    System.out.println(MAX_NODES);
+  }
 }
